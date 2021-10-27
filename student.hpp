b3df21ef2828967de0950d00d289d33530a31f9c
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-10-27 12:08:01 -0400

    Initial commit

---
+++ student.hpp
@@ -0,0 +1,16 @@
+#pragma once // prevents multiple definitions
+
+#include <iostream> // print library
+#include <string>
+using namespace std;
+
+// ===============
+// implementations
+
+//Exercise
+bool isAlphabetical(string nameOne, string nameTwo)
+{
+	return 0; // TODO: fix
+}
+
+
